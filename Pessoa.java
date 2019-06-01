b7afdadebf7e36e460c29db2d6c1a3762713a121
Walney Negreiros Gomes <36850527+walneygomes@users.noreply.github.com>
2019-06-01 13:21:42 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -2,12 +2,23 @@ package Entidades;
 
 public class Pessoa {
 
+	private ArrayList<String> s = new ArrayList<String>();
 	public void andar() {
 		System.out.print("Andando");
 	}
 
 	public void fala(String informacao) {
 		System.out.print(informacao);
+		s.add(informacao);
+	}
+	public String informcao(){
+		
+		return s.get(s.size())
+	}
+
+	
+	public void ouvindo(Pessoa pessoa) {
+		pessoa.informcao();
 	}
 
 	public void comer(Produto produto) {
